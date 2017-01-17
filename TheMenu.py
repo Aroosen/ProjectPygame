24ab21e1b07c398ef19d320540b0c9913d3e091b
Stefan Pesic <sp.pesic@live.nl>
2017-01-17 15:34:40 +0100

    Trying to set the template for the menu, added code with explanation (what they do).

---
+++ TheMenu.py
@@ -0,0 +1,15 @@
+import pygame #imports all pygame modules
+import sys #imports modules such as quit
+
+
+pygame.init() #initializes pygame
+
+fps_clock = pygame.time.Clock() #to control fps/tracks time
+
+screen = pygame.display.set_mode((640,480)) # The screen, it has to be used every time when using the screen
+pygame.display.set_caption("Welcome to the menu") #Creates the title
+pygame.display.update() #Updates the display if this is not used, no update to the screen will come
+
+white = (255,255,255) # The colour for the menu (White)
+while True:
+
